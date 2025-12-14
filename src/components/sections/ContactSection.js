86712dff31949354de85e32d4aed10de8a0312b5
joaquimmulaza <joaquimmulazadev@gmail.com>
2025-12-14 15:15:51 +0100

    change contact section

--- src/components/sections/ContactSection.js
+++ src/components/sections/ContactSection.js
@@ -153,7 +153,7 @@ const ContactSection = () => {
             >
               <h3 className="text-xl font-heading font-extrabold mb-8 text-white">Contactos</h3>
 
-              <div className="space-y-6 text-sm font-body">
+              <div className="space-y-6 text-sm font-body contact-details-list">
                 <div className="flex items-start">
                   <div className="bg-white/5 p-2 rounded-lg border border-white/10 mr-4">
                     <svg className="w-5 h-5 text-electric-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
